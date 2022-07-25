ef3f0f527bc072743ad9a265db1a4f1ed42159db
oopsHan <hanjj1024@naver.com>
2022-07-25 05:43:57 +0000

    store delete

--- Store/src/test/java/sirenorder/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package sirenorder.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import sirenorder.StoreApplication;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { StoreApplication.class })
-public class CucumberSpingConfiguration {}
