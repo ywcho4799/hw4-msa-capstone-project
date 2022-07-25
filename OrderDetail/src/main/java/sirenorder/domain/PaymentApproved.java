d55bfa9b52dd67394aca52cb095aff31198afd1d
ywcho4799 <jumtangyi@gmail.com>
2022-07-25 06:42:48 +0000

    get set 추가

--- OrderDetail/src/main/java/sirenorder/domain/PaymentApproved.java
+++ OrderDetail/src/main/java/sirenorder/domain/PaymentApproved.java
@@ -11,4 +11,35 @@ public class PaymentApproved extends AbstractEvent {
     private Long orderId;
     private Double price;
     private String payDate;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    
+    public Double getPrice(){
+        return price;
+    }
+    public void setPrice(Double price){
+        this.price = price
+    }
+   
+    public String getPayDate() {
+        return payDate;
+    }
+
+    public void setPayDate(String payDate) {
+        this.payDate = payDate;
+    }
+    
 }
--- OrderDetail/src/main/java/sirenorder/domain/PaymentCanceled.java
+++ OrderDetail/src/main/java/sirenorder/domain/PaymentCanceled.java
@@ -12,4 +12,42 @@ public class PaymentCanceled extends AbstractEvent {
     private Double price;
     private String payDate;
     private String cancelDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    
+    public Double getPrice(){
+        return price;
+    }
+    public void setPrice(Double price){
+        this.price = price
+    }
+   
+    public String getPaydate() {
+        return payDate;
+    }
+
+    public void setPayDate(String payDate) {
+        this.payDate = payDate;
+    }
+
+    public String getCancelDate() {
+        return cancelDate;
+    }
+
+    public void setCancelDate(String cancelDate) {
+        this.cancelDate = cancelDate;
+    }
 }
