dc5cdb56de52cbea1d27d30e258a80f44054da28
oopsHan <hanjj1024@naver.com>
2022-07-25 05:44:46 +0000

    store update

---
+++ Store/src/main/java/sirenorder/domain/Pickup.java
@@ -0,0 +1,85 @@
+package sirenorder.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import sirenorder.StoreApplication;
+import sirenorder.domain.PickupCanceled;
+import sirenorder.domain.PickupStarted;
+
+@Entity
+@Table(name = "Pickup_table")
+@Data
+public class Pickup {
+
+    private Long id;
+
+    private Long orderId;
+
+    private String customerAddr;
+
+    private String customerTel;
+
+    private String customerId;
+
+    private String pickupId;
+
+    private String pickupStatus;
+
+    @PostPersist
+    public void onPostPersist() {
+        PickupStarted pickupStarted = new PickupStarted(this);
+        pickupStarted.publishAfterCommit();
+
+        PickupCanceled pickupCanceled = new PickupCanceled(this);
+        pickupCanceled.publishAfterCommit();
+    }
+
+    public static PickupRepository repository() {
+        PickupRepository pickupRepository = StoreApplication.applicationContext.getBean(
+            PickupRepository.class
+        );
+        return pickupRepository;
+    }
+
+    public static void requestOrder(PaymentApproved paymentApproved) {
+        /** Example 1:  new item 
+        Pickup pickup = new Pickup();
+        repository().save(pickup);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(paymentApproved.get???()).ifPresent(pickup->{
+            
+            pickup // do something
+            repository().save(pickup);
+
+
+         });
+        */
+
+    }
+
+    public static void cancelPickup(PaymentCanceled paymentCanceled) {
+        /** Example 1:  new item 
+        Pickup pickup = new Pickup();
+        repository().save(pickup);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(paymentCanceled.get???()).ifPresent(pickup->{
+            
+            pickup // do something
+            repository().save(pickup);
+
+
+         });
+        */
+
+    }
+}
---
+++ Store/src/main/java/sirenorder/domain/PickupRepository.java
@@ -0,0 +1,9 @@
+package sirenorder.domain;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import sirenorder.domain.*;
+
+@RepositoryRestResource(collectionResourceRel = "pickups", path = "pickups")
+public interface PickupRepository
+    extends PagingAndSortingRepository<Pickup, Long> {}
