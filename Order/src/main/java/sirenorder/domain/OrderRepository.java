051b5acd1a450aed675b60537c7989e964212732
oopsHan <hanjj1024@naver.com>
2022-07-26 00:43:55 +0000

    order delete

--- Order/src/main/java/sirenorder/domain/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package sirenorder.domain;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import sirenorder.domain.*;
-
-@RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
-public interface OrderRepository
-    extends PagingAndSortingRepository<Order, Long> {}
--- Order/src/test/java/sirenorder/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package sirenorder.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import sirenorder.OrderApplication;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { OrderApplication.class })
-public class CucumberSpingConfiguration {}
